e5440e63aef422cb1014bca56a974243da9fcb61
Shivangbhatnagar237 <shivangb237@gmail.com>
2023-03-04 17:31:19 +0530

    final commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let btm = document.querySelector("#btm-part");
 
 let loader = document.querySelector(".loader");
 
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     loader.style.transform = "translateY(-100%)";
   }, 1500);
@@ -17,12 +17,27 @@ window.addEventListener("load", () => {
 });
 
 let flag = false;
+
+let sections = document.querySelectorAll(".sec");
+
+sections.forEach((sec) => {
+  sec.addEventListener("click", (e) => {
+    let url = location.href.split("#")[0];
+    clickedAgain();
+    location.href = `${url}#page-${sec.id}`;
+  });
+});
+
 var margin;
 
 const clicked = () => {
   flag = true;
   mid.style.transform = "rotate(45deg)";
-  buttons.forEach((button) => (button.style.opacity = "0"));
+  buttons.forEach((button) => {
+    if (window.matchMedia("(max-width: 480px)").matches)
+      button.style.display = "none";
+    else button.style.opacity = 0;
+  });
   lines.forEach((line) => (line.style.transform = "rotate(-45deg)"));
   web_menu.style.transform = "translateX(0%)";
   btm.style.opacity = 0;
@@ -33,7 +48,11 @@ const clickedAgain = () => {
   margin = 0;
   flag = false;
   mid.style.transform = "rotate(0)";
-  buttons.forEach((button) => (button.style.opacity = "1"));
+  buttons.forEach((button) => {
+    if (window.matchMedia("(max-width: 480px)").matches)
+      button.style.display = "flex";
+    else button.style.opacity = 1;
+  });
   lines.forEach((line) => (line.style.transform = "rotate(0)"));
   web_menu.style.transform = "translateX(-100%)";
   btm.style.opacity = 1;
@@ -66,12 +85,16 @@ decrement.addEventListener("click", () => {
 let scroller = document.querySelector(".text-scroll");
 
 web_menu.addEventListener("mousemove", (dets) => {
-  let left = dets.screenX;
-  let stripWidth = scroller.getBoundingClientRect().width;
-  let right = 1600;
-  margin = (left * stripWidth) / right;
-  console.log(margin);
-  scroller.style.marginLeft = `-${margin}px`;
+  if (!window.matchMedia("(max-width: 480px)").matches) {
+    let left = dets.screenX;
+    let stripWidth = scroller.getBoundingClientRect().width;
+    let right = 1600;
+    margin = (left * stripWidth) / right;
+    // gsap.to(scroller, {
+    //   margin: `0 0 0 -${margin}px`
+    // })
+    scroller.style.marginLeft = `-${margin}px`;
+  }
 });
 
 const scroll = new LocomotiveScroll({
@@ -123,15 +146,18 @@ main.addEventListener("scroll", (dets) => {
 });
 
 let cursor = document.querySelector(".cursor");
+
+if (window.matchMedia("(max-width: 500px)").matches) {
+  cursor.style.display = "none";
+}
+
 window.addEventListener("mousemove", (dets) => {
-  if (window.matchMedia("(max-width: 500px)").matches) {
-    cursor.style.display = "none";
-  } else {
-    cursor.style.display = "block";
-  }
-  var posX = dets.clientX - cursor.getBoundingClientRect().width / 2;
-  var posY = dets.clientY - cursor.getBoundingClientRect().height / 2;
-  cursor.style.transform = `translate(${posX}px,${posY}px)`;
+  gsap.to(cursor, {
+    x: dets.clientX,
+    y: dets.clientY,
+    duration: 0.5,
+    ease: Power4.easeOut,
+  });
 });
 
 let pageOverlay = document.querySelector(".page-overlay");
@@ -164,6 +190,8 @@ const restDetails = [
     price: "3000<sub>/day</sub>",
     dimension: "280/310<sub>sq.ft.</sub>",
     src: "./assets/luxury.webp",
+    bgsrc:
+      "https://images.pexels.com/photos/235985/pexels-photo-235985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "The Standard room, as you will see is definitely more than your average room, in terms of budget, location and service. With walls painted with bright pleasant colors and adorned with exquisite paintings, you will certainly be swept off your feet the instant you set foot here.",
   },
   {
@@ -172,6 +200,8 @@ const restDetails = [
     price: "7000<sub>/day</sub>",
     dimension: "400<sub>sq.ft.</sub>",
     src: "./assets/grand-villa.webp",
+    bgsrc:
+      "https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "Designed to give you maximum comfort and relaxation, spacious air-conditioned villa a living room, a bedroom with fabulous Jacuzzi bathtub and shower cubicles. These rooms are ideal for couples and families.",
   },
   {
@@ -180,6 +210,8 @@ const restDetails = [
     price: "5000<sub>/day</sub>",
     dimension: "350/300<sub>sq.ft.</sub>",
     src: "./assets/cottage.webp",
+    bgsrc:
+      "https://images.unsplash.com/photo-1588421357574-87938a86fa28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
     desc: "The cottages come in a state-of-the-art layout as – water body view, which overlooks the serene blue waters of the sprawling long around the roofed approach way out right opposite the garden.",
   },
   {
@@ -188,6 +220,7 @@ const restDetails = [
     price: "4000<sub>/day</sub>",
     dimension: "290<sub>sq.ft.</sub>",
     src: "./assets/business.webp",
+    bgsrc: "https://images.pexels.com/photos/352096/pexels-photo-352096.jpeg",
     desc: "For those who are looking for an added touch of personal service, the finest amenities and extra space, the Suite showcase is a spacious living room is brightly lit, with panoramic balcony that overlook spectacular scenic views.",
   },
   {
@@ -196,6 +229,7 @@ const restDetails = [
     price: "4200<sub>/day</sub>",
     dimension: "360/320<sub>sq.ft.</sub>",
     src: "./assets/resort.webp",
+    bgsrc: "https://images.pexels.com/photos/326311/pexels-photo-326311.jpeg",
     desc: "Enter the spacious grand room, tastefully done and elegantly furnished with a majestic double bed and a chest of drawers. There is a plush chair near the window from where you can enjoy sweeping views of the swimming pool & garden.",
   },
 ];
@@ -208,6 +242,7 @@ let price = document.querySelector(".price-list h1");
 let dimension = document.querySelector(".dimensions-list h1");
 let desc = document.querySelector(".res-desc h5");
 let img = document.querySelector(".gallery-viewer img");
+let hotel_view = document.querySelector(".hotel-view");
 
 const getCurrentValue = () => {
   restaurantName.textContent = restDetails[i].name;
@@ -215,6 +250,7 @@ const getCurrentValue = () => {
   price.innerHTML = restDetails[i].price;
   dimension.innerHTML = restDetails[i].dimension;
   desc.textContent = restDetails[i].desc;
+  hotel_view.style.backgroundImage = `url(${restDetails[i].bgsrc})`;
   document
     .querySelector(".gallery-viewer img")
     .setAttribute("src", restDetails[i].src);
@@ -248,18 +284,37 @@ const getCurrentValue = () => {
     { opacity: 0, ease: Expo.easeInOut },
     { opacity: 1, ease: Expo.easeInOut, duration: 1.5 }
   );
+  gsap.fromTo(
+    hotel_view,
+    { opacity: 0, ease: Expo.easeInOut },
+    { opacity: 1, ease: Expo.easeInOut, duration: 3, delay: -0.5 }
+  );
+};
+
+const mousemove = () => {
+  gsap.to(cursor, {
+    scale: 4,
+    duration: 0.3,
+  });
+};
+
+const mouseleave = () => {
+  gsap.to(cursor, {
+    scale: 1,
+    duration: 0.3,
+  });
 };
 
 const leftBtnClick = () => {
-  if (i === 0) i = restDetails.length;
+  if (i === 0) i = restDetails.length - 1;
+  else i--;
   getCurrentValue();
-  i--;
 };
 
 const rightBtnClick = () => {
-  if (i === restDetails.length) i = 0;
+  if (i === restDetails.length - 1) i = 0;
+  else i++;
   getCurrentValue();
-  i++;
 };
 
 let swiperMenu = [
@@ -301,6 +356,8 @@ let swiperMenu = [
   },
 ];
 
+let menuScroller = document.querySelector(".scroller");
+
 function show() {
   var clutter = ``;
   swiperMenu.forEach((elem, index) => {
@@ -308,7 +365,7 @@ function show() {
       clutter +
       `<div class="element">
     <div class="item-img">
-    <img src= "${elem.src}" alt=""/>
+    <img src="${elem.src}" alt="" loading="eager"/>
     </div>
     <div class="item-name">
       <h3>${elem.name}</h3>
@@ -327,36 +384,42 @@ let cards = [
     name: "Mahadev Bhojnalay",
     foodCatg: "North Indian",
     rating: "3.6",
+    time: "30 min",
   },
   {
     src: "./assets/icecream.webp",
     name: "Manohar Dairy",
     foodCatg: "North Indian",
     rating: "4.3",
+    time: "26 min",
   },
   {
     src: "./assets/poha.webp",
     name: "Sharma Vishnu",
     foodCatg: "North Indian",
     rating: "4.2",
+    time: "26 min",
   },
   {
     src: "./assets/laddoo.webp",
     name: "Kuber Sweets",
     foodCatg: "North Indian",
     rating: "4.0",
+    time: "44 min",
   },
   {
     src: "./assets/burger.webp",
     name: "Burger King",
     foodCatg: "North Indian",
     rating: "4.3",
+    time: "22 min",
   },
   {
     src: "./assets/pizza.webp",
     name: "Pizza Hut",
     foodCatg: "Italian",
     rating: "4.2",
+    time: "33 min",
   },
 ];
 
@@ -365,9 +428,12 @@ function showCards() {
   cards.forEach((elem, index) => {
     clutter =
       clutter +
-      `<div class="card">
+      `<div class="card swiper-slide">
       <div class="food-img">
-        <img src="${elem.src}" alt="">
+        <img src="${elem.src}" alt="" loading="eager">
+        <div class="time">
+        <h4>${elem.time}</h4>
+        </div>
       </div>
       <div class="text">
         <div class="head-rating">
@@ -388,3 +454,58 @@ function showCards() {
 }
 
 showCards();
+
+if (!window.matchMedia("(max-width: 480px)").matches) {
+  const lerp = (x, y, a) => x * (1 - a) + y * a;
+  let book_now = document.querySelector(".book-now");
+
+  book_now.addEventListener("mousemove", (dets) => {
+    let dims = book_now.getBoundingClientRect();
+    let xstart = dims.x;
+    let xend = dims.x + dims.width;
+    let ystart = dims.y;
+    let yend = dims.y + dims.height;
+
+    const xLerpValue = gsap.utils.mapRange(xstart, xend, 0, 1, dets.clientX);
+    const yLerpValue = gsap.utils.mapRange(ystart, yend, 0, 1, dets.clientY);
+
+    gsap.to(book_now, {
+      x: lerp(-50, 50, xLerpValue),
+      y: lerp(-50, 50, yLerpValue),
+      duration: 0.3,
+    });
+
+    gsap.to(cursor, {
+      scale: 8,
+      duration: 0.3,
+    });
+  });
+
+  book_now.addEventListener("mouseleave", () => {
+    gsap.to(book_now, {
+      x: 0,
+      y: 0,
+      duration: 0.3,
+    });
+    gsap.to(cursor, {
+      scale: 1,
+      duration: 0.3,
+    });
+  });
+}
+
+let food_cards = document.querySelector(".cards");
+if (window.matchMedia("(max-width: 480px)").matches) {
+  gsap.fromTo(
+    ".card",
+    {
+      y: "40%",
+    },
+    {
+      y: "-560%",
+      repeat: -1,
+      duration: 10,
+      ease: Power0.easeOut,
+    }
+  );
+}
